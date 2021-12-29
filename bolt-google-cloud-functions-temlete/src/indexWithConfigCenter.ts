2d14f08d1330999a7359d012580e68889e264388
黄晓琳 Serlin Huang (Engineering - FE) <xiaolin.huang@aftership.com>
2021-12-29 16:23:59 +0800

    perf: ⚡️ upgrade readme and congig center template

---
+++ bolt-google-cloud-functions-temlete/src/indexWithConfigCenter.ts
@@ -0,0 +1,34 @@
+import { App, LogLevel } from '@slack/bolt'
+import { ConfigCenter } from '@aftership/config-center-sdk'
+import { config } from 'dotenv'
+
+config()
+let configCenter
+const handleEvent = async () => {
+  const config_center = new ConfigCenter()
+  configCenter = await config_center.init()
+
+  const app = new App({
+    token: configCenter['slack.bot-token'],
+    appToken: configCenter['slack.app-token'],
+    signingSecret: configCenter['slack.signing-secret'],
+    logLevel: LogLevel.DEBUG,
+  })
+
+  const submitter = await app.client.users.lookupByEmail({
+    token: process.env.SLACK_BOT_TOKEN,
+    email: 'xx@aftership.com',
+  })
+  const submitterId = submitter.user.id
+
+  try {
+    app.client.chat.postMessage({
+      channel: submitterId,
+      text: `Welcome to the team, <@${submitterId}>! 🎉 You can introduce yourself in this channel.`,
+    })
+  } catch (err) {
+    console.error(err, 'err')
+  }
+}
+
+export { handleEvent }
